2dea836ff0c39b8329b5c37c06d3d9c600e5586a
Nemo <Nemo@NEMO>
2020-07-03 20:06:57 +0300

    created
    app structure and added scss

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
 
+import Cards from "./components/Cards/Cards";
+import Chart from "./components/Chart/Chart";
+import CountryPicker from "./components/CountryPicker/CountryPicker";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </header>
     </div>
   );
 }
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class CountryPicker extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Country Picker</h1>
+      </div>
+    );
+  }
+}
+
+export default CountryPicker;
